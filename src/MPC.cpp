7fa31b0c015e15dd4e393bc634ea4ba22eb313ae
Chris Manning <cmanning@vt.edu>
2018-05-20 19:32:30 -0400

    update weights

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -56,21 +56,21 @@ class FG_eval {
 
     // The part of the cost based on the reference state.
     for (unsigned int t = 0; t < N; ++t) {
-      fg[0] += pow(10, 0) * CppAD::pow(vars[cte_start + t], 2);
-      fg[0] += pow(10, 3) * CppAD::pow(vars[epsi_start + t], 2);
-      fg[0] += pow(10, 0) * CppAD::pow(vars[v_start + t] - ref_v, 2);
+      fg[0] += 1 * pow(10, 0) * CppAD::pow(vars[cte_start + t], 2);
+      fg[0] += 1 * pow(10, 3) * CppAD::pow(vars[epsi_start + t], 2);
+      fg[0] += 1 * pow(10, 0) * CppAD::pow(vars[v_start + t] - ref_v, 2);
     }
 
     // Minimize the use of actuators.
     for (unsigned int t = 0; t < N - 1; ++t) {
-      fg[0] += pow(10, 2) * CppAD::pow(vars[delta_start + t], 2);
-      fg[0] += pow(10, 2) * CppAD::pow(vars[a_start + t], 2);
+      fg[0] += 5 * pow(10, 2) * CppAD::pow(vars[delta_start + t], 2);
+      fg[0] += 1 * pow(10, 2) * CppAD::pow(vars[a_start + t], 2);
     }
 
     // Minimize the value gap between sequential actuations.
     for (unsigned int t = 0; t < N - 2; ++t) {
-      fg[0] += pow(10, 2) * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
-      fg[0] += pow(10, 0) * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
+      fg[0] += 6 * pow(10, 2) * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 1 * pow(10, 0) * CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
     //
